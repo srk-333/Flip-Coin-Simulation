a7e60028efdafa45ab1b8ead80d13ddfbcf30ee4
srk-333 <sauravmk333@gmail.com>
2021-12-29 19:16:38 +0530

    Added: Displaying winner

--- FlipCoinSimu.sh
+++ FlipCoinSimu.sh
@@ -1,3 +1,13 @@
-#!/bin/bash -
+#!/bin/bash -x
 
 echo "Welcome to Coin Flip Simulation Program"
+
+flipCheck=$(( RANDOM%2 ))
+isHead=1;
+
+if [[ $isHead -eq $flipCheck ]]
+    then 
+        echo "Head is winner"
+    else 
+         echo "Tail is winner"
+fi
