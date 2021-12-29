56a5e568ed2f3a3ddad73f15a7cda4f328a5201e
srk-333 <sauravmk333@gmail.com>
2021-12-29 19:26:23 +0530

    Added: Coin flipped Multiple Times

--- FlipCoinSimu.sh
+++ FlipCoinSimu.sh
@@ -2,12 +2,25 @@
 
 echo "Welcome to Coin Flip Simulation Program"
 
-flipCheck=$(( RANDOM%2 ))
+read -p "Enter numer for  fliping coin" n;
 isHead=1;
+HCount=0;
+TCount=0;
 
-if [[ $isHead -eq $flipCheck ]]
-    then 
-        echo "Head is winner"
-    else 
-         echo "Tail is winner"
-fi
+for (( i=0;i<$n;i++ ))
+ do
+        flipCheck=$(( RANDOM%2 ));
+
+        if [[ $isHead -eq $flipCheck ]]
+            then 
+                     let HCount++
+             else 
+                     let TCount++
+       fi
+done
+
+echo "Number of times Head won = "$HCount;
+
+echo "Number of times Tail  won = "$TCount;
+
+
